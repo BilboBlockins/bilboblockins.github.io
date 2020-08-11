7d983ffc6d3ca908d5cf61f82b101dc31e89c195
bilboblockins <abretthiebert@gmail.com>
2020-08-11 17:07:45 -0400

    update img path

--- double/utils/check-images.js
+++ double/utils/check-images.js
@@ -39,5 +39,3 @@ function checkImgs(array) {
   }
   return problems
 }
-
-
---
+++ double/utils/tidy-imgs.js
@@ -0,0 +1,19 @@
+//Node script to remove images that were removed from actor data due to bad face read
+//Run after getting new doubles model
+const fs = require('fs')
+const removeData = require('../data/doubles_to_remove.json')
+
+run()
+
+function run() {
+  //check if all actors have associated images
+  removeImgs(removeData)
+  console.log('All bad images removed...')
+  console.log('Done')
+}
+
+function removeImgs(array) {
+  for(let actor of array) {
+
+  }
+}
